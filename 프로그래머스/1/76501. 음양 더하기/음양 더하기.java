cbeb1cf2057eccbc05aacf4dd690deb148ad099a
Mojin2 <70631068+Mojin2@users.noreply.github.com>
2024-07-19 17:31:14 +0900

    [level 1] Title: 음양 더하기, Time: 5.15 ms, Memory: 81.6 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
@@ -1,10 +1,15 @@
+import java.util.stream.IntStream;
+
 class Solution {
     public int solution(int[] absolutes, boolean[] signs) {
-        int answer = 0;
-        for (int i = 0 ;i < absolutes.length; i++){
-            if (signs[i] == false) answer -= absolutes[i];
-            else answer += absolutes[i];
-        }
-        return answer;
+        return IntStream.range(0,signs.length).map(i->
+                                           {int si = signs[i]? 1:-1;
+                                           return absolutes[i]*si;}).sum();
+        // int answer = 0;
+        // for (int i = 0 ;i < absolutes.length; i++){
+        //     if (signs[i] == false) answer -= absolutes[i];
+        //     else answer += absolutes[i];
+        // }
+        // return answer;
     }
 }
