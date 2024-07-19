8008668976de3c365bfb4ed1621d0c4b6b368c55
Mojin2 <70631068+Mojin2@users.noreply.github.com>
2024-07-19 15:53:24 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 1.10 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -3,14 +3,15 @@ import java.util.stream.*;
 
 class Solution {
     public int solution(int[] numbers) {
-        int answer = 0;
-        int[] index = {0,1,2,3,4,5,6,7,8,9};
-        for (int i =0 ;i < index.length;i++){
-            for (int j =0 ;j < numbers.length; j++){
-                if (index[i] == numbers[j]) break;
-                if (j == numbers.length-1) answer += index[i];
-            }
-        }
-        return answer;
+        // int answer = 0;
+        // int[] index = {0,1,2,3,4,5,6,7,8,9};
+        // for (int i =0 ;i < index.length;i++){
+        //     for (int j =0 ;j < numbers.length; j++){
+        //         if (index[i] == numbers[j]) break;
+        //         if (j == numbers.length-1) answer += index[i];
+        //     }
+        // }
+        // return answer;
+        return 45-Arrays.stream(numbers).sum();
     }
 }
