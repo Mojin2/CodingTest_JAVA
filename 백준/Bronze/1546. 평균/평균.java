354c232a458aafc8bd3f2ae26fdb138bdf1e036a
Mojin2 <70631068+Mojin2@users.noreply.github.com>
2024-08-06 16:11:41 +0900

    [Bronze I] Title: 평균, Time: 200 ms, Memory: 19032 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int N = Integer.parseInt(st.nextToken());
+		int[] arr = new int[N];
+		st = new StringTokenizer(br.readLine());
+
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+
+		int max = Arrays.stream(arr).max().getAsInt();
+		double answer = Arrays.stream(arr).mapToDouble(el -> (double) el / max * 100).sum() / N;
+
+		System.out.println(answer);
+
+	}
+
+}
