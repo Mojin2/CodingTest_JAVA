853f13a27515f42ac13d8a3e630d5c96509120e3
Mojin2 <70631068+Mojin2@users.noreply.github.com>
2024-08-22 01:37:44 +0900

    [Gold V] Title: Z, Time: 60 ms, Memory: 11536 KB -BaekjoonHub

--- 백준/Gold/1074. Z/Z.java
+++ 백준/Gold/1074. Z/Z.java
@@ -1,50 +1,88 @@
 import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    static int r, c;
-    static int count = 0;
+	static int count;
+	static int r;
+	static int c;
+	static String[][] board;
 
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(st.nextToken());
-        r = Integer.parseInt(st.nextToken());
-        c = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(st.nextToken());
+		r = Integer.parseInt(st.nextToken());
+		c = Integer.parseInt(st.nextToken());
+		count = 0;
 
-        int size = (int) Math.pow(2, N);
-        findOrder(size, 0, 0);
-    }
+		int t = (int) Math.pow(2, N);
 
-    private static void findOrder(int size, int row, int col) {
-        if (size == 1) {
-            System.out.println(count);
-            return;
-        }
+		dv(0, 0, t);
+	}
 
-        int newSize = size / 2;
+	private static void dv(int x, int y, int size) {
+		if (size == 1) {
+			if (x == r && y == c) {
+				System.out.println(count);
+			}
+			count++;
+			return;
+		}
 
-        // 1사분면
-        if (r < row + newSize && c < col + newSize) {
-            findOrder(newSize, row, col);
-        } 
-        // 2사분면
-        else if (r < row + newSize && c >= col + newSize) {
-            count += newSize * newSize;
-            findOrder(newSize, row, col + newSize);
-        } 
-        // 3사분면
-        else if (r >= row + newSize && c < col + newSize) {
-            count += 2 * newSize * newSize;
-            findOrder(newSize, row + newSize, col);
-        } 
-        // 4사분면
-        else {
-            count += 3 * newSize * newSize;
-            findOrder(newSize, row + newSize, col + newSize);
-        }
-    }
+		int t = size / 2;
+
+		// 1사분면 (왼쪽 위)
+		if (r < x + t && c < y + t) {
+			dv(x, y, t);
+		}
+		// 2사분면 (오른쪽 위)
+		else if (r < x + t && c >= y + t) {
+			count += t * t;
+			dv(x, y + t, t);
+		}
+		// 3사분면 (왼쪽 아래)
+		else if (r >= x + t && c < y + t) {
+			count += 2 * t * t;
+			dv(x + t, y, t);
+		}
+		// 4사분면 (오른쪽 아래)
+		else {
+			count += 3 * t * t;
+			dv(x + t, y + t, t);
+		}
+	}
+//	private static void dv(int x, int y, int t) {
+//		
+//		int size = t;
+//		
+//		if (x > r || r >= x + t || y > c || c >= y + t) {
+//			count += t * t;
+//			return;
+//		}
+//
+//		if (x <= r && r < x + t && y <= c && c < y + t) {
+//			for (int i = x; i < x + t; i++) {
+//				for (int j = y; j < y + t; j++) {
+//					if (i == r && j == c) {
+//						System.out.println(count);
+//					} else {
+//						count++;
+//					}
+//				}
+//			}
+//
+//			return;
+//		}
+//
+//		dv(x, y, size / 2);
+//		dv(x + size / 2, y, size / 2);
+//		dv(x, y + size / 2, size / 2);
+//		dv(x + size / 2, y + size / 2, size / 2);
+
 }
