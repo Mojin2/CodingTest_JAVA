7770c2ae43aac2fe60705a6737ceac5b54806b71
Mojin2 <70631068+Mojin2@users.noreply.github.com>
2024-09-02 15:53:13 +0900

    [Gold IV] Title: 작업, Time: 488 ms, Memory: 79960 KB -BaekjoonHub

---
+++ 백준/Gold/2056. 작업/작업.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int N = Integer.parseInt(st.nextToken());
+		ArrayList<Integer>[] works = new ArrayList[N];
+		for (int i = 0; i < N; i++) {
+			works[i] = new ArrayList<>();
+		}
+
+		int[] dp = new int[N + 1];
+		int[] times = new int[N + 1];
+
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			int time = Integer.parseInt(st.nextToken());
+			if (i == 0) {
+				dp[1] = time;
+			}
+			times[i + 1] = time;
+			int cnt = Integer.parseInt(st.nextToken());
+			for (int j = 0; j < cnt; j++) {
+				works[i].add(Integer.parseInt(st.nextToken()));
+			}
+		}
+
+		int answer = dp[1];
+
+		for (int i = 1; i < works.length; i++) {
+			int max = Integer.MIN_VALUE;
+			if (works[i].size() == 0) {
+				dp[i + 1] = times[i + 1];
+				continue;
+			}
+			for (Integer tmp : works[i]) {
+				max = Math.max(max, dp[tmp]);
+			}
+			dp[i + 1] = max + times[i + 1];
+			answer = Math.max(answer, dp[i + 1]);
+		}
+		System.out.println(answer);
+	}
+}
