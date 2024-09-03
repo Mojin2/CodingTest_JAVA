cd37f795e6b9e3f8e82c76739aed919d17f86288
Mojin2 <70631068+Mojin2@users.noreply.github.com>
2024-09-03 17:45:29 +0900

    [Gold III] Title: 중량제한, Time: 552 ms, Memory: 53692 KB -BaekjoonHub

--- 백준/Gold/1939. 중량제한/중량제한.java
+++ 백준/Gold/1939. 중량제한/중량제한.java
@@ -1,86 +1,76 @@
-import java.io.*;
-import java.util.*;
- 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
 public class Main {
-    static int atoi(String str) {
-        return Integer.parseInt(str);
-    }
-    static int N, M;
-    static ArrayList<Node> A = new ArrayList<>();
-    static int start, end, max = Integer.MIN_VALUE;
-    static int [] parent;
-    public static void main(String[] args) throws IOException {
-        input();
-        pro();
-    }
- 
-    static void pro() {
-        Collections.sort(A, (o1, o2)->{
-            return o2.wei - o1.wei;
-        });
- 
-        for (Node n : A) {
-            union(n.from, n.to);
-            if(find(start) == find(end)){
-                System.out.println(n.wei);
-                return;
-            }
-        }
-    }
- 
-    static void union(int u, int v) {
-        u = find(u);
-        v = find(v);
- 
-        if(u == v) return;
- 
-        if(u > v){
-            parent[u] = v;
-        }
-        else{
-            parent[v] = u;
-        }
-    }
- 
-    static int find(int v) {
-        if(v == parent[v]) return v;
- 
-        parent[v] = find(parent[v]);
-        return parent[v];
-    }
- 
- 
-    static void input() throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
- 
-        N = atoi(st.nextToken());
-        M = atoi(st.nextToken());
- 
-        parent = new int[N+1];
-        for(int i = 1; i <= N; i++) parent[i] = i;
- 
-        for (int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine());
-            int from = atoi(st.nextToken());
-            int to = atoi(st.nextToken());
-            int wei = atoi(st.nextToken());
- 
-            A.add(new Node(from, to, wei));
-        }
- 
-        st = new StringTokenizer(br.readLine());
-        start = atoi(st.nextToken());
-        end = atoi(st.nextToken());
-    }
- 
-    static class Node {
-        int from, to, wei;
- 
-        public Node(int from, int to, int wei) {
-            this.from = from;
-            this.to = to;
-            this.wei = wei;
-        }
-    }
+
+	static int N;
+	static int M;
+	static int[] parent;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		ArrayList<int[]> queue = new ArrayList<>();
+
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int from = Integer.parseInt(st.nextToken());
+			int to = Integer.parseInt(st.nextToken());
+			int w = Integer.parseInt(st.nextToken());
+			queue.add(new int[] { from, to, w });
+		}
+
+		Collections.sort(queue, (a, b) -> b[2] - a[2]);
+
+		parent = new int[N + 1];
+		for (int i = 0; i < N + 1; i++) {
+			parent[i] = i;
+		}
+
+		st = new StringTokenizer(br.readLine());
+		int start = Integer.parseInt(st.nextToken());
+		int des = Integer.parseInt(st.nextToken());
+
+		for (int[] cur : queue) {
+			int cx = cur[0];
+			int cy = cur[1];
+			int cw = cur[2];
+			union(cx, cy);
+			if (find(start) == find(des)) {
+				System.out.println(cw);
+				break;
+			}
+		}
+	}
+
+	static void union(int u, int v) {
+		u = find(u);
+		v = find(v);
+
+		if (u == v)
+			return;
+
+		if (u > v) {
+			parent[u] = v;
+		} else {
+			parent[v] = u;
+		}
+	}
+
+	static int find(int v) {
+		if (v == parent[v])
+			return v;
+
+		parent[v] = find(parent[v]);
+		return parent[v];
+	}
 }
