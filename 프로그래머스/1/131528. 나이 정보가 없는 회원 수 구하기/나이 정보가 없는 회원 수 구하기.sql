2e6cc0458f82038d4d2b398c4a97543ebcd4585f
Mojin2 <70631068+Mojin2@users.noreply.github.com>
2024-09-09 10:53:08 +0900

    [level 1] Title: 나이 정보가 없는 회원 수 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/131528. 나이 정보가 없는 회원 수 구하기/나이 정보가 없는 회원 수 구하기.sql
+++ 프로그래머스/1/131528. 나이 정보가 없는 회원 수 구하기/나이 정보가 없는 회원 수 구하기.sql
@@ -1,8 +1,8 @@
 -- 코드를 입력하세요
-# SELECT count(AGE is null) as USERS
-# from USER_INFO
-
-
-SELECT count(*) as USERS
+SELECT count(CASE WHEN AGE IS NULL THEN USER_ID END) as USERS
 from USER_INFO
-where AGE IS NULL
+
+
+# SELECT count(*) as USERS
+# from USER_INFO
+# where AGE IS NULL
