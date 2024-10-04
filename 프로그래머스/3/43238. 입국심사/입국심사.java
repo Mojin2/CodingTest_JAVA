fc0514ad8d7e0eac09d8a425169f8a74ee0138ea
Mojin2 <70631068+Mojin2@users.noreply.github.com>
2024-10-04 21:02:48 +0900

    [level 3] Title: 입국심사, Time: 57.36 ms, Memory: 82.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -0,0 +1,36 @@
+class Solution {
+    public static long solution(int n, int[] times) {
+		long answer = 0;
+		long max = Integer.MIN_VALUE;
+
+		for (int i = 0; i < times.length; i++) {
+			max = Math.max(max, times[i]);
+		}
+
+		long start = 1;
+		long end = max*n;
+
+		while (start <= end) {
+			long mid = (start + end) / 2;
+//			System.out.println(mid);
+
+			long count = 0;
+			for (int i = 0; i < times.length; i++) {
+				count += mid / times[i];
+			}
+
+//			if (count == n) {
+//				System.out.println(mid);
+//				return mid;
+//			}
+
+			if (count >= n) {
+				end = mid - 1;
+			} else {
+				start = mid + 1;
+			}
+		}
+		//System.out.println(end+1);
+		return end+1;
+	}
+}
