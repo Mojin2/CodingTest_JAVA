e7c28e9a1714e6abb76b7ff2c98d9a432cd13eca
Mojin2 <70631068+Mojin2@users.noreply.github.com>
2024-10-21 20:15:05 +0900

    [Silver III] Title: 수열, Time: 176 ms, Memory: 23268 KB -BaekjoonHub

---
+++ 백준/Silver/2559. 수열/수열.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		
+		int[] arr = new int[N];
+		
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0 ;i <N;i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		
+		int[] cum = new int[N];
+		cum[0]  = arr[0];
+		
+		for (int i = 1; i<N;i++) {
+			cum[i] = cum[i-1] + arr[i]; 
+		}
+		
+		
+		int max = cum[K-1];
+		
+		for (int i = K; i<N;i++) {
+			max = Math.max(max, cum[i]-cum[i-K]);
+		}
+		
+		System.out.println(max);
+	}
+}
