31e252f96806963995af09437c6da2d3e2b24c45
Mojin2 <70631068+Mojin2@users.noreply.github.com>
2024-11-04 11:51:05 +0900

    [Silver III] Title: 주유소, Time: 280 ms, Memory: 40744 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -1,68 +1,48 @@
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
+	
+	static int N;
+	static int M;
+	static int[] arrN;
+	static int[] arrM;
+	
+	public static void main(String[] args) throws IOException{
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-
-		int N = Integer.parseInt(st.nextToken());
-
-		int[] distance = new int[N - 1];
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		
+		N = Integer.parseInt(br.readLine());	// 도시 수
+		M = N-1;								// 도로 수
+		
+		arrN = new int[N+1];					// 도시별 기름 가격
+		arrM = new int[M+1];					// 도로의 길이 배열
+		
 		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < N - 1; i++) {
-			distance[i] = Integer.parseInt(st.nextToken());
+		for(int i=1; i<=M; i++) {
+			arrM[i] = Integer.parseInt(st.nextToken());
 		}
 
-		int[] price = new int[N];
 		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < N; i++) {
-			price[i] = Integer.parseInt(st.nextToken());
-		}
-
-//		System.out.println(Arrays.toString(distance));
-//		System.out.println(Arrays.toString(price));
-
-		int totalPrice = 0;
-
-		int pc = 0;
-
-//		4
-//		2 3 1
-//		5 2 4 1
-
-		for (int i = 0; i < N - 1; i++) {
-			int curDis = distance[i];
-			int tmp = curDis;
-			int curPrice = price[i];
-//			System.out.println("curPrice:::"+curPrice);
-
-			for (int j = i+1; j < N; j++) {
-				if (j == N - 1) {
-					totalPrice += curPrice * tmp;
-//					System.out.println("0:::"+totalPrice);
-					tmp =0;
-					i= j-1;
-					break;
-				}
-
-				int nextPrice = price[j];
-				if (curPrice >= nextPrice) {
-					totalPrice += curPrice * tmp;
-//					System.out.println("1:::"+totalPrice);
-					tmp= 0;
-					i = j-1;
-					break;
-				} else {
-					tmp += distance[j];
-				}
-			}
+		for(int i=1; i<=N; i++) {
+			arrN[i] = Integer.parseInt(st.nextToken());	
 		}
 		
-		System.out.println(totalPrice);
+		long minCost = arrN[1];
+		long amt = 0;
+		
+		for(int i=1; i<=N-1; i++) {
+			if(arrN[i] < minCost) {
+				minCost = arrN[i];
+			}
+			
+			amt += minCost * arrM[i];
+		}
+		
+		System.out.println(amt);
 	}
 }
