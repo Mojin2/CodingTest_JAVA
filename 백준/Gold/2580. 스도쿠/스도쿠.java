ccf1ed3e6b5ecd32aee5b8171d8c9728f60ba9ba
Mojin2 <70631068+Mojin2@users.noreply.github.com>
2024-11-18 15:17:19 +0900

    [Gold IV] Title: 스도쿠, Time: 240 ms, Memory: 15884 KB -BaekjoonHub

--- 백준/Gold/2580. 스도쿠/스도쿠.java
+++ 백준/Gold/2580. 스도쿠/스도쿠.java
@@ -6,85 +6,69 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int[][] board;
-    static boolean solved;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static int[][] board;
+	static boolean check;
+	static boolean[][] rowUsed;
+	static boolean[][] colUsed;
+	static boolean[][][] boxUsed;
 
-        board = new int[9][9];
-        solved = false;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        for (int i = 0; i < 9; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < 9; j++) {
-                board[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
+		board = new int[9][9];
+		check = false;
+		rowUsed = new boolean[9][10];  // 각 행에 사용된 숫자 추적
+		colUsed = new boolean[9][10];  // 각 열에 사용된 숫자 추적
+		boxUsed = new boolean[3][3][10];  // 각 3x3 박스에 사용된 숫자 추적
 
-        backtrack(0, 0); // 시작 위치를 지정하여 순회
-    }
+		for (int i = 0; i < 9; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < 9; j++) {
+				board[i][j] = Integer.parseInt(st.nextToken());
+				if (board[i][j] != 0) {  // 초기 값이 있는 경우 사용된 숫자로 마킹
+					rowUsed[i][board[i][j]] = true;
+					colUsed[j][board[i][j]] = true;
+					boxUsed[i / 3][j / 3][board[i][j]] = true;
+				}
+			}
+		}
 
-    private static void backtrack(int row, int col) {
-        if (row == 9) { // 모든 행을 다 채운 경우
-            printBoard();
-            solved = true;
-            return;
-        }
+		backtrack(0, 0);
+	}
 
-        if (board[row][col] != 0) { // 이미 숫자가 있는 칸은 다음 칸으로 이동
-            nextCell(row, col);
-        } else {
-            boolean[] list = canUse(row, col);
+	private static void backtrack(int x, int y) {
+		if (x == 9) {  // 보드를 다 채운 경우
+			StringBuilder sb = new StringBuilder();
+			for (int i = 0; i < 9; i++) {
+				for (int j = 0; j < 9; j++) {
+					sb.append(board[i][j]).append(" ");
+				}
+				sb.append("\n");
+			}
+			System.out.println(sb);
+			check = true;
+			return;
+		}
 
-            for (int num = 1; num <= 9; num++) {
-                if (list[num]) {
-                    board[row][col] = num;
-                    nextCell(row, col); // 다음 칸으로 이동
-                    if (solved) return; // 이미 해결된 경우 중지
-                    board[row][col] = 0; // 백트래킹: 숫자 제거
-                }
-            }
-        }
-    }
+		if (board[x][y] != 0) {  // 이미 채워진 칸인 경우 다음 칸으로 이동
+			backtrack(y == 8 ? x + 1 : x, (y + 1) % 9);
+			return;
+		}
 
-    private static void nextCell(int row, int col) {
-        if (col == 8) {
-            backtrack(row + 1, 0);
-        } else {
-            backtrack(row, col + 1);
-        }
-    }
+		// 1~9 숫자를 시도
+		for (int num = 1; num <= 9; num++) {
+			if (!rowUsed[x][num] && !colUsed[y][num] && !boxUsed[x / 3][y / 3][num]) {
+				board[x][y] = num;
+				rowUsed[x][num] = colUsed[y][num] = boxUsed[x / 3][y / 3][num] = true;
 
-    private static boolean[] canUse(int x, int y) {
-        boolean[] list = new boolean[10];
-        Arrays.fill(list, true);
+				backtrack(y == 8 ? x + 1 : x, (y + 1) % 9);
+				if (check) return;  // 답을 찾은 경우
 
-        for (int i = 0; i < 9; i++) {
-            list[board[x][i]] = false; // 가로
-            list[board[i][y]] = false; // 세로
-        }
-
-        int startX = 3 * (x / 3);
-        int startY = 3 * (y / 3);
-
-        for (int i = startX; i < startX + 3; i++) {
-            for (int j = startY; j < startY + 3; j++) {
-                list[board[i][j]] = false;
-            }
-        }
-
-        return list;
-    }
-
-    private static void printBoard() {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                sb.append(board[i][j]).append(" ");
-            }
-            sb.append("\n");
-        }
-        System.out.print(sb);
-    }
+				// 되돌리기
+				board[x][y] = 0;
+				rowUsed[x][num] = colUsed[y][num] = boxUsed[x / 3][y / 3][num] = false;
+			}
+		}
+	}
 }
