937a5c02c5ce5b32b22816c397f4ced85a5d1e56
Mojin2 <70631068+Mojin2@users.noreply.github.com>
2024-12-10 17:17:30 +0900

    [Silver V] Title: 숫자 카드, Time: 788 ms, Memory: 172724 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -1,49 +1,32 @@
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
-//5
-//6 3 2 10 -10
-//8
-//10 9 -5 2 3 4 5 -10
-
-// 1. 리스트의 contain메서드를 활용한 방법 -> 시간초과
-// 2. 어떻게 풀까 map?
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-		int N = Integer.parseInt(st.nextToken());
-		int[] have = new int[N];
-		ArrayList<Integer> have2 = new ArrayList<>();
-		HashMap<Integer, Integer> have3 = new HashMap<>();
+        int N = Integer.parseInt(st.nextToken());
+        HashSet<Integer> haveSet = new HashSet<>();
 
-		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < N; i++) {
-			int num = Integer.parseInt(st.nextToken());
-			have[i] = num;
-			have2.add(num);
-			have3.put(num, 1);
-		}
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            haveSet.add(Integer.parseInt(st.nextToken()));
+        }
 
-		st = new StringTokenizer(br.readLine());
-		int M = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        int M = Integer.parseInt(st.nextToken());
 
-		StringBuilder sb = new StringBuilder();
-		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < M; i++) {
-			int num = Integer.parseInt(st.nextToken());
-			if (have3.get(num) != null) {
-				sb.append("1").append(" ");
-			} else {
-				sb.append("0").append(" ");
-			}
-		}
-		System.out.println(sb);
-	}
+        StringBuilder sb = new StringBuilder();
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            if (haveSet.contains(num)) {
+                sb.append("1").append(" ");
+            } else {
+                sb.append("0").append(" ");
+            }
+        }
+        System.out.println(sb);
+    }
 }
