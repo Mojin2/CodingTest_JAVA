df68af043bad9437b2032be49f3db2f2d43ffed5
Mojin2 <70631068+Mojin2@users.noreply.github.com>
2024-12-14 18:03:10 +0900

    [Silver II] Title: 최대 힙, Time: 276 ms, Memory: 30460 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -0,0 +1,49 @@
+//13
+//0
+//1
+//2
+//0
+//0
+//3
+//2
+//1
+//0
+//0
+//0
+//0
+//0
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int N = Integer.parseInt(st.nextToken());
+		PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			int num = Integer.parseInt(st.nextToken());
+			if (num == 0) {
+				if (pq.size() == 0) {
+					sb.append(0).append("\n");
+				} else {
+					sb.append(pq.poll()).append("\n");
+				}
+			} else {
+				pq.add(num);
+			}
+		}
+
+		System.out.println(sb);
+	}
+}
