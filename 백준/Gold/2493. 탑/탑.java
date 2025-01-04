6786bce0d478288e9db15ff1cae4f39c4d6c34dc
Mojin2 <70631068+Mojin2@users.noreply.github.com>
2025-01-04 15:04:50 +0900

    [Gold V] Title: 탑, Time: 672 ms, Memory: 113624 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -2,55 +2,48 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Arrays;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
-//5
-//6 9 5 7 4
-
+// BOJ 2493 - 탑
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        int[] answer = new int[N];
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
 
-		int N = Integer.parseInt(st.nextToken());
+        Stack<Integer> stack = new Stack<>();
 
-		int[] arr = new int[N];
-		st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            int height = arr[i];
 
-		for (int i = 0; i < N; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
+            // 현재 탑의 높이보다 작은 탑들은 모두 제거
+            while (!stack.isEmpty() && arr[stack.peek()] < height) {
+                stack.pop();
+            }
 
-		ArrayDeque<Integer> stack = new ArrayDeque<>();
+            // 신호를 수신할 탑이 존재하는지 체크
+            if (stack.isEmpty()) {
+                answer[i] = 0;
+            } else {
+                answer[i] = stack.peek() + 1;  // 1-based index
+            }
 
-		ArrayDeque<Integer> answer = new ArrayDeque<>();
+            stack.push(i);  // 현재 탑을 스택에 추가
+        }
 
-		for (int i = 0; i < N; i++) {
-			int height = arr[i];
-
-			if (stack.isEmpty()) {
-				stack.add(i);
-				answer.add(0);
-			} else {
-				while (arr[stack.peekLast()] < height) {
-					stack.pollLast();
-					if (stack.isEmpty())
-						break;
-				}
-				if (stack.isEmpty()) {
-					stack.add(i);
-					answer.add(0);
-				} else {
-					answer.add(stack.peekLast() + 1);
-					stack.add(i);
-				}
-
-			}
-		}
-		for (Integer num : answer) {
-			System.out.print(num + " ");
-		}
-	}
+        // 결과 출력
+        StringBuilder sb = new StringBuilder();
+        for (int num : answer) {
+            sb.append(num).append(" ");
+        }
+        System.out.println(sb);
+    }
 }
